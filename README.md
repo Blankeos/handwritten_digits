1cfc9de58149901a8c250a79ab5e36a267188502
Blankeos <carloantonioct@gmail.com>
2022-01-03 02:05:10 +0800

    Fixed one not being recognized bug

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ A beginner handwriting deeplearning model to practice Computer Vision + Deeplear
 
 Also made a **PyGame** to apply the model detection in real time.
 
-Based on [Edureka's tutorial](https://www.youtube.com/watch?v=L2cAjgc1-bo) on the same dataset
+Based on [Edureka's tutorial](https://www.youtube.com/watch?v=L2cAjgc1-bo) on the same dataset.d
 
 ![Preview Image](/preview.gif)
--- app.py
+++ app.py
@@ -11,7 +11,7 @@ WINDOW_SIZE_Y = 480
 WHITE = (255,255,255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
-ALLOW_IMAGE_SAVE = False
+ALLOW_IMAGE_SAVE = True
 MODEL = load_model("bestmodel.h5")
 LABEL_NAMES = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"}
 BOUNDARYINC = 5
@@ -42,50 +42,53 @@ while True:
 
         if event.type == MOUSEBUTTONUP:
             iswriting = False
-            number_xcord = sorted(number_xcord)
-            number_ycord = sorted(number_ycord)
+            if (not(len(number_xcord) <= 0 or len(number_xcord) <= 0)):
+                number_xcord = sorted(number_xcord)
+                number_ycord = sorted(number_ycord)
 
-            rect_min_x, rect_max_x = max(number_xcord[0] - BOUNDARYINC, 0), min(WINDOW_SIZE_X, number_xcord[-1]+BOUNDARYINC)
+                rect_min_x, rect_max_x = max(number_xcord[0] - BOUNDARYINC, 0), min(WINDOW_SIZE_X, number_xcord[-1]+BOUNDARYINC)
 
-            rect_min_y, rect_max_y = max(number_ycord[0] - BOUNDARYINC, 0), min(number_ycord[-1]+BOUNDARYINC, WINDOW_SIZE_X)
+                rect_min_y, rect_max_y = max(number_ycord[0] - BOUNDARYINC, 0), min(number_ycord[-1]+BOUNDARYINC, WINDOW_SIZE_X)
 
-            number_xcord = []
-            number_ycord = []
+                number_xcord = []
+                number_ycord = []
 
-            img_arr = np.array(pygame.PixelArray(DISPLAY_SURFACE))[rect_min_x:rect_max_x, rect_min_y:rect_max_y].T.astype(np.float32)
+                img_arr = np.array(pygame.PixelArray(DISPLAY_SURFACE))[rect_min_x:rect_max_x, rect_min_y:rect_max_y].T.astype(np.float32)
 
-            if ALLOW_IMAGE_SAVE:
-                cv2.imwrite("image.png")
-                image_count += 1
+                if ALLOW_IMAGE_SAVE:
+                    image_to_save = np.pad(img_arr, (15,15), 'constant', constant_values=0)
+                    image_to_save = cv2.resize(image_to_save, (28,28))
+                    cv2.imwrite("image.png", image_to_save)
+                    image_count += 1
 
-            if PREDICT:
-                image = cv2.resize(img_arr, (28,28))
-                image = np.pad(image, (10,10), 'constant', constant_values=0)
-                image = cv2.resize(image, (28,28))/255
+                if PREDICT:
+                    image = np.pad(img_arr, (15,15), 'constant', constant_values=0)
+                    image = cv2.resize(image, (28,28))
+                    image = cv2.resize(image, (28,28))/255
 
-                label = str(LABEL_NAMES[np.argmax(MODEL.predict(image.reshape(1,28,28,1)))])
+                    prediction = MODEL.predict(image.reshape(1,28,28,1))
+                    winning_index = np.argmax(prediction)
+                    label = f"{str(LABEL_NAMES[winning_index])}"
 
-                textSurface = FONT.render(label, True, RED, WHITE)
-                textRecObj = textSurface.get_rect()
+                    textSurface = FONT.render(label, True, RED, WHITE)
+                    textRecObj = textSurface.get_rect()
 
-                # Centering algorithm
-                imageSizeX = rect_max_x - rect_min_x
-                textSizeX = textRecObj.size[0]
-                xOffset = (imageSizeX - textSizeX) / 2
-                textRecObj.left, textRecObj.top = rect_min_x + xOffset, rect_max_y
+                    # Centering algorithm
+                    imageSizeX = rect_max_x - rect_min_x
+                    textSizeX = textRecObj.size[0]
+                    xOffset = (imageSizeX - textSizeX) / 2
+                    textRecObj.left, textRecObj.top = rect_min_x + xOffset, rect_max_y
 
-                # top line
-                pygame.draw.line(DISPLAY_SURFACE, RED, (rect_min_x, rect_min_y), (rect_max_x, rect_min_y), 3)
-                # bottom line
-                pygame.draw.line(DISPLAY_SURFACE, RED, (rect_min_x, rect_max_y), (rect_max_x, rect_max_y), 3)
-                # left line
-                pygame.draw.line(DISPLAY_SURFACE, RED, (rect_min_x, rect_min_y), (rect_min_x, rect_max_y), 3)
-                # right line
-                pygame.draw.line(DISPLAY_SURFACE, RED, (rect_max_x, rect_min_y), (rect_max_x, rect_max_y), 3)
+                    # top line
+                    pygame.draw.line(DISPLAY_SURFACE, RED, (rect_min_x, rect_min_y), (rect_max_x, rect_min_y), 3)
+                    # bottom line
+                    pygame.draw.line(DISPLAY_SURFACE, RED, (rect_min_x, rect_max_y), (rect_max_x, rect_max_y), 3)
+                    # left line
+                    pygame.draw.line(DISPLAY_SURFACE, RED, (rect_min_x, rect_min_y), (rect_min_x, rect_max_y), 3)
+                    # right line
+                    pygame.draw.line(DISPLAY_SURFACE, RED, (rect_max_x, rect_min_y), (rect_max_x, rect_max_y), 3)
 
-                DISPLAY_SURFACE.blit(textSurface, textRecObj)
-
-
+                    DISPLAY_SURFACE.blit(textSurface, textRecObj)
 
         if event.type == MOUSEMOTION and iswriting:
             xcord, ycord = event.pos
